10c340e1b84f7c8e846a4def40ee0a325f1efb06
Manish Saraswat <manisara0000@gmail.com>
2024-12-30 07:43:22 +0100

    Update nodeIntersection.py

--- Linked_List/nodeIntersection.py
+++ Linked_List/nodeIntersection.py
@@ -2,6 +2,9 @@
 Intersection of Two Linked Lists
 https://leetcode.com/problems/intersection-of-two-linked-lists/
 
+Check this comment for visual approach: 
+https://leetcode.com/problems/intersection-of-two-linked-lists/solutions/49785/java-solution-without-knowing-the-difference-in-len/comments/165648
+
 listA = [1,9,1,2,4], listB = [3,2,4]
 """
 # Definition for singly-linked list.
