803ef0e01a36b3bbc689c1d0963f7023f4f411d9
Manish Saraswat <manisara0000@gmail.com>
2025-02-23 19:15:18 +0530

    Update revenueMilestones.py

--- meta-25/revenueMilestones.py
+++ meta-25/revenueMilestones.py
@@ -16,7 +16,30 @@ class Solution:
     TC: O(N + MlogN) N <- length of revenue, M <- length of milestones
     SC: O(N + M)
     """
-    pass
+    N = len(revenues)
+    cum_revenue = revenues[:]
+    for i in range(1, len(revenues)):
+      cum_revenue[i] += cum_revenue[i-1]
+
+    def search(arr, target):
+      l = 0
+      r = len(arr) - 1
+
+      while l < r:
+        mid = (l+r)//2
+        if arr[mid] >= target:
+          r = mid
+        else:
+          l = mid+1
+      return l
+
+    ans = []
+    # for each milestone for a binary search M log N
+    for m in milestones:
+      idx = search(revenues, m)
+      ans.append(idx+1 if idx != N else -1)
+      
+    return ans
 
   def solutionTwo(self, revenues, milestones):
     """
@@ -24,8 +47,18 @@ class Solution:
     TC: O(N + MlogM), N <- length of revenue, M <- length of milestones
     SC: O(N)
     """
-    pass
-
+    M= len(milestones)
+    res = [0] * M
+    hashmap = {c:i for i, c in enumerate(milestones)}
+    
+    k = 0
+    for i, r in enumerate(revenues):
+      total_revenue += r
+      while k < M and total_revenue <= milestones[k]:
+        res[hashmap[milestones[k]]] = i + 1
+        k += 1
+    return res
+    
 
 if __name__ == "__main__":
   revenues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
